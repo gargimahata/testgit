ce261e7e60195c36cc24cd0f3ae1c00448f1310d
gargimahata <gargimahata25@gmail.com>
2022-07-23 13:41:34 +0530

    this code

---
+++ mongodb.py
@@ -0,0 +1,11 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://gargi4u:gargi4u@cluster0.oza10tb.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+d = {'name':'gargi',
+     'saurnme':'mahata',
+     'mail':'g@gmail.com'}
+
+db1= client['mongotest']
+coll = db1['test']
+coll.insert_one(d )
